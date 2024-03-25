a273bf259a986659a9fb6316bc9b2c834680981d
Nael Ayman <naelayman95.na@gmail.com>
2024-03-25 15:30:35 +0200

    get cart list, cart length, items prices, total price

--- src/app/carts/carts.module.ts
+++ src/app/carts/carts.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { CartsRoutingModule } from './carts-routing.module';
 import { CartsComponent } from './components/carts/carts.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { CartsComponent } from './components/carts/carts.component';
   ],
   imports: [
     CommonModule,
-    CartsRoutingModule
+    CartsRoutingModule,
+    SharedModule
   ]
 })
 export class CartsModule { }
--- src/app/carts/components/carts/carts.component.ts
+++ src/app/carts/components/carts/carts.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-carts',
   templateUrl: './carts.component.html',
   styleUrls: ['./carts.component.scss']
 })
-export class CartsComponent {
+export class CartsComponent implements OnInit {
+
+  cartProducts: any[] = [];
+  total: any = 0;
+
+  ngOnInit(): void {
+    this.getCartList();
+  }
+
+  getCartList() {
+    if('Cart' in localStorage) {
+      this.cartProducts = JSON.parse(localStorage.getItem('Cart') !);
+    }
+    console.log(this.cartProducts);
+    this.getCartTotalPrice();
+  }
+
+  getCartTotalPrice() {
+    this.total = 0;
+    for(let x in this.cartProducts) {
+      this.total += this.cartProducts[x].item.price * this.cartProducts[x].quantity;
+    }
+  }
 
 }
